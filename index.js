6293c72b11036f7c8e60931b3a994fee7df518dd
emil.huo <emil.huo@gmail.com>
2018-09-30 13:20:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+ return document.getElementById("app").querySelector(" #nested .target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll("ul.ranked-list li");
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+  
+}
+
+function deepestChild() {
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+  }
 
